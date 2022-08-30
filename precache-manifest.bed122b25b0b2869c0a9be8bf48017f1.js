c5a88ba5b413f7d66f4f8f2b35d97cfc85f14def
ruchitachandhok <chandhok.ruchita@gmail.com>
2022-08-29 22:02:01 -0400

    Updates

---
+++ precache-manifest.bed122b25b0b2869c0a9be8bf48017f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1121bb46d9101694695f",
+    "url": "/ruchitachandhok/project-4-assessment.git/static/css/main.07787def.chunk.css"
+  },
+  {
+    "revision": "1121bb46d9101694695f",
+    "url": "/ruchitachandhok/project-4-assessment.git/static/js/main.1121bb46.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/ruchitachandhok/project-4-assessment.git/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "1fb84335d4ce8f26b1b6",
+    "url": "/ruchitachandhok/project-4-assessment.git/static/js/2.1fb84335.chunk.js"
+  },
+  {
+    "revision": "c8b270734d6f31b05abd",
+    "url": "/ruchitachandhok/project-4-assessment.git/static/js/runtime~main.c8b27073.js"
+  },
+  {
+    "revision": "36d3e31b6637d68281e244747f39e02f",
+    "url": "/ruchitachandhok/project-4-assessment.git/index.html"
+  }
+];
